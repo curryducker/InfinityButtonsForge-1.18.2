cf7308c8147bb08f9f75b1d1836a6d444ad8f6a1
curryducker <jur.hoekman@gmail.com>
2023-06-25 11:18:24 +0200

    dispense

--- src/main/java/net/larsmans/infinitybuttons/InfinityButtons.java
+++ src/main/java/net/larsmans/infinitybuttons/InfinityButtons.java
@@ -9,8 +9,15 @@ import net.larsmans.infinitybuttons.compat.*;
 import net.larsmans.infinitybuttons.config.InfinityButtonsConfig;
 import net.larsmans.infinitybuttons.config.InfinityButtonsConfigMenu;
 import net.larsmans.infinitybuttons.item.InfinityButtonsItems;
+import net.larsmans.infinitybuttons.item.custom.SafeEmergencyButtonItem;
 import net.larsmans.infinitybuttons.network.IBPacketHandler;
 import net.larsmans.infinitybuttons.sounds.InfinityButtonsSounds;
+import net.minecraft.core.BlockSource;
+import net.minecraft.core.dispenser.OptionalDispenseItemBehavior;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.DispenserBlock;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -19,6 +26,7 @@ import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -92,5 +100,16 @@ public class InfinityButtons
     private void setup(final FMLCommonSetupEvent event)
     {
         IBPacketHandler.register();
+
+        for (Item safe : ForgeRegistries.ITEMS.getValues()) {
+            if (safe instanceof SafeEmergencyButtonItem) {
+                DispenserBlock.registerBehavior(safe, new OptionalDispenseItemBehavior() {
+                    protected ItemStack execute(BlockSource blockSource, ItemStack itemStack) {
+                        this.setSuccess(ArmorItem.dispenseArmor(blockSource, itemStack));
+                        return itemStack;
+                    }
+                });
+            }
+        }
     }
 }
--- src/main/java/net/larsmans/infinitybuttons/item/custom/SafeEmergencyButtonItem.java
+++ src/main/java/net/larsmans/infinitybuttons/item/custom/SafeEmergencyButtonItem.java
@@ -9,6 +9,7 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import org.jetbrains.annotations.Nullable;
 
 public class SafeEmergencyButtonItem extends BlockItem {
     public SafeEmergencyButtonItem(Block pBlock, Properties pProperties) {
@@ -18,6 +19,11 @@ public class SafeEmergencyButtonItem extends BlockItem {
     @Override
     public boolean canEquip(ItemStack stack, EquipmentSlot armorType, Entity entity) {
         return armorType == EquipmentSlot.HEAD;
+    }
+
+    @Override
+    public @Nullable EquipmentSlot getEquipmentSlot(ItemStack stack) {
+        return EquipmentSlot.HEAD;
     }
 
     @Override
